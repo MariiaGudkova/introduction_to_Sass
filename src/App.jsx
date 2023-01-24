1d149a2a1e6674ba6aa00aa6e20c848b0a0b2c96
Gudkova Mariia <gudmary8@gmail.com>
2023-01-24 15:13:24 +0400

    work with mixins and functions

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,37 @@ function App() {
         </p>
       </main>
       <aside className="grid__sidebar">
-        <h2>Больше информации</h2>
-        <h3>—ฅ/ᐠ. ̫ .ᐟ\ฅ —</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div className="grid__widget grid__widget_green">
+          <h2>Больше информации</h2>
+          <h3>—ฅ/ᐠ. ̫ .ᐟ\ฅ —</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className="grid__widget grid__widget_red">
+          <h2>Еще больше информации</h2>
+          <h3>—ฅ/ᐠ. ̫ .ᐟ\ฅ —</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div className="grid__widget">
+          <h2 className="yellow">Еще-еще больше информации</h2>
+          <h3>—ฅ/ᐠ. ̫ .ᐟ\ฅ —</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </aside>
     </div>
   );
